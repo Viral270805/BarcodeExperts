5604a682a60670a3a69c131fc51832e80d5445e9
Viral270805 <viralmjain270805@gmail.com>
2025-03-30 11:26:11 +0530

    testing lcd

--- display.py
+++ display.py
@@ -1,22 +1,26 @@
 import RPi.GPIO as GPIO
-import time
 import smbus
+import time
 
-# Set GPIO mode to BCM
+# Set GPIO numbering mode
 GPIO.setmode(GPIO.BCM)
 
-# I2C address (Check using i2cdetect -y 1)
+# Define I2C Address (Check using i2cdetect -y 1)
 I2C_ADDR = 0x27  # Change to 0x3F if needed
 bus = smbus.SMBus(1)
 
-# LCD Commands
-LCD_CHR = 1  # Sending data
-LCD_CMD = 0  # Sending command
-LCD_LINE_1 = 0x80  # 1st line
-LCD_LINE_2 = 0xC0  # 2nd line
+# LCD Constants
+LCD_CHR = 1  # Data mode
+LCD_CMD = 0  # Command mode
+LCD_LINE_1 = 0x80  # First line
+LCD_LINE_2 = 0xC0  # Second line
 ENABLE = 0b00000100  # Enable bit
 
-# Send command to LCD
+# LED Pin
+LED_PIN = 3
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+# Function to send data/command to LCD
 def lcd_byte(bits, mode):
     high_bits = mode | (bits & 0xF0) | ENABLE
     low_bits = mode | ((bits << 4) & 0xF0) | ENABLE
@@ -33,38 +37,33 @@ def lcd_init():
     lcd_byte(0x01, LCD_CMD)
     time.sleep(0.5)
 
-# Send string to LCD
+# Display message on LCD
 def lcd_string(message, line):
     lcd_byte(line, LCD_CMD)
     for char in message:
         lcd_byte(ord(char), LCD_CHR)
 
-# Setup GPIO 3 for LED (Same as your sample code)
-LED_PIN = 3
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-# Main function
+# Main Function
 def main():
     lcd_init()
 
     try:
         while True:
-            # Display First Message
-            lcd_string("Hello, User!", LCD_LINE_1)
-            lcd_string("Raspberry Pi", LCD_LINE_2)
+            # Display message while LED blinks
+            lcd_string("Project Running", LCD_LINE_1)
+            lcd_string("LED Blinking...", LCD_LINE_2)
             GPIO.output(LED_PIN, GPIO.HIGH)  # LED ON
             time.sleep(1)
 
-            # Display Second Message
-            lcd_string("I2C LCD Ready!", LCD_LINE_1)
-            lcd_string("Enjoy!", LCD_LINE_2)
-            GPIO.output(LED_PIN, GPIO.LOW)   # LED OFF
+            lcd_string("16x2 LCD Ready", LCD_LINE_1)
+            lcd_string("Raspberry Pi!", LCD_LINE_2)
+            GPIO.output(LED_PIN, GPIO.LOW)  # LED OFF
             time.sleep(1)
 
     except KeyboardInterrupt:
         pass
 
-    # Cleanup GPIO on exit
+    # Cleanup GPIO
     GPIO.cleanup()
 
 if __name__ == "__main__":
